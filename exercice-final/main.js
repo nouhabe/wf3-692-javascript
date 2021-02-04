247a4705339c5f733ab558da1d5fcec158243867
Nouha Chemingui <nouha.bettaieb@avanade.com>
2021-02-04 09:08:35 +0100

    suite exercice-final

--- exercice-final/main.js
+++ exercice-final/main.js
@@ -5,16 +5,27 @@ window.onload = function(){
     conteneur.setAttribute('class', 'container');
     body.appendChild(conteneur);
     var numberOfCard = 25;
-    for(i=0 ; i < numberOfCard ; i++){
-        let card =document.createElement('div');
-        card.setAttribute('class', 'carte');
-        conteneur.appendChild(card);
-        card.addEventListener('click' , function(e){
-            alert ("la Div est bien cliquable");
-            console.log(element);}
-        )
-    }
 
+
+ 
+ var couleur =['FFFFFF','FF0000','0000FF','008000','FFFF00','000000']
+//  var color = getArrayRandomElement(couleur);
+ 
+function random(couleur) {
+     return couleur[Math.floor(Math.random() * couleur.length)]
+}
+random(couleur);
+
+    for(i=0 ; i < numberOfCard ; i++){
+        let card = new Image();
+        card.src = 'https://via.placeholder.com/150.png?text=Mistery+card';
+        card.setAttribute('class', 'carte');
+        conteneur.appendChild(card); 
+        card.onclick = function(){
+            card.src = 'https://via.placeholder.com/150.png/'+random(couleur)+'?text=Mistery+card';
+               
+        }  
+    }
 
 
 
